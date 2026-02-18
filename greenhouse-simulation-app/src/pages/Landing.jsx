6039289130642e8cd55b1bacb5f40112d0caf453
Sovannratana Khek <ratanakhek22@gmail.com>
2026-02-17 18:12:46 -0600

    Update to landing page

---
+++ greenhouse-simulation-app/src/pages/Landing.jsx
@@ -0,0 +1,10 @@
+function Landing() {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <p>This is the landing page.</p>
+    </div>
+  )
+}
+
+export default Landing
---
+++ greenhouse-simulation-app/src/pages/Simulation.jsx
@@ -0,0 +1,10 @@
+function Simulation() {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <p>This is the simulation page.</p>
+    </div>
+  )
+}
+
+export default Simulation
